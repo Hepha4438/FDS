518e73d0c75e46f52af7f55ff2a0e1298c24cf32
Do Hepha <trantrunghieudo@gmail.com>
2026-09-10 13:03:06 +0700

    Initial commit

--- src_cuda/run_usher.py
+++ src_cuda/run_usher.py
@@ -70,9 +70,9 @@ class LandmarkCrossAttentionTransform(nn.Module):
 
         # 🌟 THÊM MỚI: Mạng MLP phi tuyến hóa (2 lớp)
         self.mlp = nn.Sequential(
-            nn.Linear(output_dim, 256),
+            nn.Linear(output_dim, 512),
             nn.GELU(),
-            nn.Linear(256, output_dim)
+            nn.Linear(512, output_dim)
         )
         # Khởi tạo MLP tiệm cận Identity để không gây sốc ở Epoch đầu
         nn.init.zeros_(self.mlp[2].weight)
